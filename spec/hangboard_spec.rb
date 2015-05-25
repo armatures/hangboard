c3974970d77b18fbef1c5a2989ecb708907fbbe1
Charlie Bevis <charliebevis@users.noreply.github.com>
2015-05-24 18:45:21 -0700

    the test asserts that one_hold consists of calling one_rep 6 times

--- spec/hangboard_spec.rb
+++ spec/hangboard_spec.rb
@@ -4,11 +4,12 @@ require 'rspec'
 describe Hangboard do
   describe "workout" do
    it "counts down" do
+    subject = Hangboard.new
     waiter = object_double(Waiter.new, :wait => true)
     allow(Waiter).to receive(:new).and_return(waiter)
 
-    expect(waiter).to receive(:wait)
-    Hangboard.new.workout
+    expect(subject).to receive(:one_rep).exactly(6).times
+    subject.one_hold
    end
   end
 end
